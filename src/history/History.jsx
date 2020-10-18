b3e7d9299da30c43c1cc9fe77e12aa4f4733ac04
Guichi Zhao <zgc910619@gmail.com>
2020-10-18 12:02:24 +0800

    Shuttle in out (#8)
    Co-authored-by: guichi <guichi.zhao@zerologix.com>

--- src/history/History.jsx
+++ src/history/History.jsx
@@ -3,44 +3,54 @@ import HistoryItem from './HistoryItem'
 import historyStyles from './history.module.scss'
 import useStyle from '../component/useStyle'
 import Accordion from '../component/Accordion'
+import { useTranslation } from 'react-i18next'
+import histories from '../data/mock/historyDB'
 
-const status = ['all', 'success', 'failed', 'pending']
+const FILTERS = ['all', 'success', 'pending']
+
 export default function History() {
   const [cx] = useStyle(historyStyles)
+  const { t } = useTranslation('history')
   const [expanded, setExpanded] = useState(false)
-  const [current, setCurrent] = useState(status[0])
-  return <div className={cx('history-container')}>
-    <div className={cx('select')}>
-      <Accordion
-        expanded={expanded}
-        title={
-          <div
-            className={cx('select-item')}
-            onClick={() => setExpanded(x => !x)}>{current}</div>
-        }
-        content={
-
-          <div className={cx('select-content')}>
-            {
-              status.map(status => (<div
-                onClick={() => {
-                  setExpanded(false)
-                  setCurrent(status)
-                }}
-                key={status}
-                className={cx('select-item', 'hover', { active: status === current })}>
-                {status}
-              </div>))
-            }
-          </div>
-        } />
+  const [current, setCurrent] = useState(FILTERS[0])
+  return (
+    <div className={cx('history-container')}>
+      <div className={cx('select')}>
+        <Accordion
+          expanded={expanded}
+          title={
+            <div
+              className={cx('select-item')}
+              onClick={() => setExpanded((x) => !x)}
+            >
+              {t(current)}
+            </div>
+          }
+          content={
+            <div className={cx('select-content')}>
+              {FILTERS.map((status) => (
+                <div
+                  onClick={() => {
+                    setExpanded(false)
+                    setCurrent(status)
+                  }}
+                  key={status}
+                  className={cx('select-item', 'hover', {
+                    active: status === current,
+                  })}
+                >
+                  {t(status)}
+                </div>
+              ))}
+            </div>
+          }
+        />
+      </div>
+      <div className={cx('history-items')}>
+        {histories.map((props) => {
+          return <HistoryItem {...props} />
+        })}
+      </div>
     </div>
-    <div className={cx('history-items')}>
-      <HistoryItem />
-      <HistoryItem />
-      <HistoryItem />
-      <HistoryItem />
-      <HistoryItem />
-    </div>
-  </div>
+  )
 }
