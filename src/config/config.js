1d65c32d9edcbdb62bc66627e7c61346b033f03a
guichi <guichi.zhao@zerologix.com>
2020-10-28 15:29:57 +0800

    feat: edit config

--- src/config/config.js
+++ src/config/config.js
@@ -5,24 +5,27 @@ export let EHTHERSCAN_URL,
   CONFLUXSCAN_TX,
   CONFLUXSCAN_TK,
   CUSTODIAN_CONTRACT_ADDR,
-  NODE_URL,
-  SPONSOR_URL
+  NODE_URL = '/rpcshuttleflow',
+  SPONSOR_URL = '/rpcsponsor'
+
 
 //dev
-if (true) {
-  EHTHERSCAN_URL = 'https://rinkeby.etherscan.io'
-  CONFLUXSCAN_URL = 'https://confluxscan.io'
-  NODE_URL = '/rpcshuttleflow'
-  SPONSOR_URL = '/rpcsponsor'
-  CUSTODIAN_CONTRACT_ADDR = '0x897c185209ed461070db137ae34b18f467bef8a8'
-} else {
-  //prod
-  EHTHERSCAN_URL = 'https://etherscan.io'
-  EHTHERSCAN_URL = 'https://confluxscan.io'
-}
+// if (true) {
+//   EHTHERSCAN_URL = 'https://rinkeby.etherscan.io'
+//   CONFLUXSCAN_URL = 'http://scantest.confluxnetwork.org'
+//   CUSTODIAN_CONTRACT_ADDR = '0x897c185209ed461070db137ae34b18f467bef8a8'
+// } else {
+//   //prod
+//   EHTHERSCAN_URL = 'https://etherscan.io'
+//   EHTHERSCAN_URL = 'https://confluxscan.io'
+// }
+
+EHTHERSCAN_URL = 'https://etherscan.io'
+EHTHERSCAN_URL = 'https://confluxscan.io'
+CUSTODIAN_CONTRACT_ADDR = 'TBA'
 
 EHTHERSCAN_TX = EHTHERSCAN_URL + '/tx/'
 EHTHERSCAN_TK = EHTHERSCAN_URL + '/token/'
 
-CONFLUXSCAN_TX = CONFLUXSCAN_URL + '/transactionsdetail/'
+CONFLUXSCAN_TX = CONFLUXSCAN_URL + '/transaction/'
 CONFLUXSCAN_TK = CONFLUXSCAN_URL + '/token/'
