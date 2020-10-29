29ef2f5d7444bf2252c68fe1af9ad158b130628f
Guichi Zhao <zgc910619@gmail.com>
2020-10-29 10:18:18 +0800

    feat: edit config (#27)
    * fix: translation; turn off auto complete
    
    * fix: "address" name is the reason trigger autocomplete
    
    * add proxy
    
    * fix: sponsor
    
    * feat: edit config
    
    * fix: add test CUSTODIAN
    
    Co-authored-by: guichi <guichi.zhao@zerologix.com>

--- src/config/config.js
+++ src/config/config.js
@@ -1,23 +1,32 @@
+let CONFLUXSCAN_URL
 export let EHTHERSCAN_URL,
   EHTHERSCAN_TX,
-  CONFLUXSCAN_URL,
+  EHTHERSCAN_TK,
   CONFLUXSCAN_TX,
+  CONFLUXSCAN_TK,
   CUSTODIAN_CONTRACT_ADDR,
-  NODE_URL,
-  SPONSOR_URL
+  NODE_URL = '/rpcshuttleflow',
+  SPONSOR_URL = '/rpcsponsor'
+
 
 //dev
-if (true) {
-  EHTHERSCAN_URL = 'https://rinkeby.etherscan.io'
-  CONFLUXSCAN_URL = 'https://confluxscan.io'
-  NODE_URL = '/rpcshuttleflow'
-  SPONSOR_URL = '/rpcsponsor'
-  CUSTODIAN_CONTRACT_ADDR = '0x897c185209ed461070db137ae34b18f467bef8a8'
-} else {
-  //prod
-  EHTHERSCAN_URL = 'https://etherscan.io'
-  EHTHERSCAN_URL = 'https://confluxscan.io'
-}
+// if (true) {
+//   EHTHERSCAN_URL = 'https://rinkeby.etherscan.io'
+//   CONFLUXSCAN_URL = 'http://scantest.confluxnetwork.org'
+//   CUSTODIAN_CONTRACT_ADDR = '0x897c185209ed461070db137ae34b18f467bef8a8'
+// } else {
+//   //prod
+//   EHTHERSCAN_URL = 'https://etherscan.io'
+//   EHTHERSCAN_URL = 'https://confluxscan.io'
+// }
+
+EHTHERSCAN_URL = 'https://etherscan.io'
+EHTHERSCAN_URL = 'https://confluxscan.io'
+//production env
+CUSTODIAN_CONTRACT_ADDR = '0x890e3feac4a2c33d7594bc5be62e7970ef5481e0'
 
 EHTHERSCAN_TX = EHTHERSCAN_URL + '/tx/'
-CONFLUXSCAN_TX = CONFLUXSCAN_URL + '/transactionsdetail/'
+EHTHERSCAN_TK = EHTHERSCAN_URL + '/token/'
+
+CONFLUXSCAN_TX = CONFLUXSCAN_URL + '/transaction/'
+CONFLUXSCAN_TK = CONFLUXSCAN_URL + '/token/'
