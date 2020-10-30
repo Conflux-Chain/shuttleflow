29f09d57ea9139dfefb5ca154f221bfaf0cd9c8e
guichi <guichi.zhao@zerologix.com>
2020-10-30 18:54:43 +0800

    fix return null

--- src/history/HistoryItem.jsx
+++ src/history/HistoryItem.jsx
@@ -137,6 +137,8 @@ export default function HistoryItem(props) {
                             }}
                           />
                         )
+                      } else {
+                        return null
                       }
                     })()}
                 </div>
